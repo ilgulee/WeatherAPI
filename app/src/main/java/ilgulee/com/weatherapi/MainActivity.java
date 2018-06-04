fe6f12e559cfcca2acae406d68019a1e3280833c
Ilgu Lee <iglee2000ca@gmail.com>
2018-06-04 01:36:52 -0400

    Add Async OkHttp get method, error handling, AlertDialogFragment, Network connectivity check

--- app/src/main/java/ilgulee/com/weatherapi/MainActivity.java
+++ app/src/main/java/ilgulee/com/weatherapi/MainActivity.java
@@ -1,13 +1,85 @@
 package ilgulee.com.weatherapi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String apiKey = "5d11e6b89bc94407aa6d06e2801c8fe4";
+        String forecastURL = "https://api.darksky.net/forecast/" + apiKey +
+                "/" + latitude + "," + longitude;
+
+        if (isNetworkAvailable()){
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastURL)
+                    .build();
+
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        String jsonData = response.body().string();
+                        if (response.isSuccessful()) {
+                            Log.v(TAG, jsonData);
+                        }else{
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "IO Exception caught: ", e);
+                    }
+
+                }
+            });
+            Log.d(TAG, "Main UI code is running, hooray!");
+        }else{
+            Toast.makeText(this, R.string.network_unavailable_message,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
